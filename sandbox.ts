8fe080d09433f5b25c6df066fbbbe88d7dd33227
dodytrifa <dody.trifa@gmail.com>
2021-07-31 22:44:05 +0700

    learn array object

--- sandbox.ts
+++ sandbox.ts
@@ -2,8 +2,18 @@ const character = 'mario';
 
 console.log(character);
 
+//character = 90 //* akan error karena diubah typenya
+
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
   console.log(input);
 });
+
+//*function, param harus diberi type
+
+const circle = (diameter: number)=> {
+  return diameter * Math.PI
+}
+
+console.log(circle(9));
