7b4df1065cf783bbc7d35565d85a302d49ef315b
dodytrifa <dody.trifa@gmail.com>
2021-08-31 22:58:50 +0700

    setup buttons

--- vuets/src/App.vue
+++ vuets/src/App.vue
@@ -1,13 +1,38 @@
 <template>
-<div class="app">Halo User!</div>
+  <div class="app"><p>{{name}} - {{age}}</p>
+    <button @click="changeName()">
+      change name
+    </button>
+    <button @click="changeAge()">
+      change age
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
+
+
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      name: 'Link', //* initiate string type
+      age: 25 as number | string //* union type
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: number){ //*bisa salah satu refer dari union type
+      this.age = age
+      return age
+    }
   }
 });
 </script>
