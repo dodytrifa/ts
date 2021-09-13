8e6551614872156b099ea34973cbffe9b076daab
dodytrifa <dody.trifa@gmail.com>
2021-09-13 21:25:39 +0700

    learn hooks with TS

--- tsreactsample1/src/TextFieldComp.tsx
+++ tsreactsample1/src/TextFieldComp.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
 
 interface Props {
   text: string;
+  ok?: boolean //* tanda ? artinya optional
+  fn?: (anotherText: string) => string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+interface TextNode {
+  textNode: string;
 }
 
 //* REact.FC = funct component
-export const TextField: React.FC<Props> = () => { //* component texfield memiliki props text tipe string
+export const TextField: React.FC<Props> = ({handleChange}) => { //* component texfield memiliki props di interface, text tipe string
+
+  //*Hooks
+  const  [count, setCount] = useState<number | null | undefined>(5); //* 5 sebagai initial state
+
+  setCount(null)
+  
+  //*bentuk 2
+  // const [display, setDisplay] = useState<TextNode>({textNode: 'hello'});
+
+  // setDisplay({textNode});
+
+  //*useRef Hooks
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input  />
+    <div ref={divRef}>
+      <input  ref={inputRef} onChange={handleChange}/>
     </div>
   )
 }
