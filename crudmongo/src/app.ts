c1f1413e57dee62bf22d8963fb7073e3b5e76f4f
dodytrifa <dody.trifa@gmail.com>
2021-09-21 20:17:00 +0700

    setup config

--- crudmongo/src/app.ts
+++ crudmongo/src/app.ts
@@ -1,1 +1,20 @@
-console.log("Halo");
+// console.log("Halo");
+
+import express from 'express';
+import config from 'config';
+
+const port = config.get("port") as number;
+const host = config.get("host") as string;
+
+const app = express();
+
+app.use(express.json())
+//* karena versi express baru, shg tidak perlu install body parser
+//* app use sbg middleware
+
+app.use(express.urlencoded({extended: false}))
+
+app.listen(port, host, () => {
+  console.log(`server start at http://${host}:${port}`);
+  
+})
