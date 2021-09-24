39b22e36037f496025783aa5045ff0ef77d01e34
dodytrifa <dody.trifa@gmail.com>
2021-09-24 22:15:33 +0700

    learn function

---
+++ _nbsts/02-function.ts
@@ -0,0 +1,46 @@
+function addNumber(a:number,b:number){
+  return a + b;
+}
+
+//* export default practice di TS
+
+export const addStrings = (str:string, str2:string = "") : string => `${str} ${str2}`
+
+//*union type
+export const format = (title:string, param: string | number ): string | number => `${title} ${param}`
+
+//*ganti target tsconfig dulu karena masih es5
+export const fetchData = (url:string): Promise<string> => 
+  Promise.resolve(`Data from ${url}`)
+ 
+function introduce(salutation: string, ...names: string[]): string {
+  return `${salutation} ${names.join(" ")}`
+}
+
+//?-----------FUNCTION RETURN FUNCTION
+export function printFile(text:string, callback:()=> void): void {
+  console.log(text);
+  callback()
+}
+
+//*bentuk pertama = verbose
+/*
+export function mutating(
+  numbers: number[], 
+  mutate: (value:number)=> number)
+  : number[]{
+  return numbers.map(mutate);
+}
+*/
+
+type MutateFn = (value:number) => number;
+
+export function mutating(
+  numbers: number[], 
+  mutate: MutateFn
+): number[]{
+  return numbers.map(mutate);
+}
+
+
+export default addNumber
---
+++ _nbsts/02a-function.ts
@@ -0,0 +1,14 @@
+import addNumber, {addStrings,fetchData,format,mutating} from "./02-function";
+
+console.log(addNumber(5,4));
+console.log(addStrings("Hallo","User"));
+
+//* param 2 bisa tidak diisi karena di function diberi default parameter
+console.log(addStrings("Hallo", ));
+console.log(format("Hai", 7));
+console.log(fetchData("fetch"));
+
+//?FUNCTION RETURN FUNCTION
+console.log(mutating([8,9,9], (value) => value * 10 ));
+
+
