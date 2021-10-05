7493964e406692399ba3f3a3e45fdd3a3e24b12f
dodytrifa <dody.trifa@gmail.com>
2021-10-05 21:08:02 +0700

    generic sample #2

--- _nbsts/08-genericsKeyof.ts
+++ _nbsts/08-genericsKeyof.ts
@@ -9,3 +9,34 @@ const players = [
 
 console.log(pluck(players, "age"));
 console.log(pluck(players, "name"));
+
+
+//? ============= EXAMPLE 2 ===============
+
+interface BaseEvent {
+  time: number,
+  user: string
+}
+
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string}
+  checkout: BaseEvent
+}
+
+function sendEvent<Name extends keyof EventMap>(name: Name, data: EventMap[Name],data2: EventMap[Name]):void{
+  console.log([name, data,data2]);
+}
+
+sendEvent("addToCart",{
+  productID: "idProduct",
+  user: "userProperty",
+  quantity: 1,
+  time:100
+},
+{
+  productID: "idProduct2",
+  user: "userProperty2",
+  quantity: 2,
+  time:200
+}
+)
