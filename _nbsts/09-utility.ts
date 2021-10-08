738f8cfc74936f1250faac824eb6addd98fc03e0
dodytrifa <dody.trifa@gmail.com>
2021-10-08 22:45:25 +0700

    learn more utility

--- _nbsts/09-utility.ts
+++ _nbsts/09-utility.ts
@@ -2,15 +2,19 @@ interface MyUser {
   name: string,
   id: string,
   email?: string
+  phone?: string
 }
 
-interface MyUserOptions {
-  name?: string,
-  id?: string,
-  email?: string
-}
+type MyUserOptionsPartial = Partial<MyUser>
 
-const merge = (user: MyUser, overrides: MyUserOptions): MyUser => {
+//? BENTUK PERTAMA SEBELUM PARTIAL
+// interface MyUserOptions {
+//   name?: string,
+//   id?: string,
+//   email?: string
+// }
+
+const merge = (user: MyUser, overrides: MyUserOptionsPartial): MyUser => {
   return {
     ...user,
     ...overrides
@@ -22,3 +26,31 @@ console.log(merge({
   id: "johnID",
   email: "sample@mail.com"
 }, {email: "thelatest@mail.com"}));
+
+//? REQUIRED
+//? dengan gunakan rquired maka semua field dalam MyUser harus disertakan, tidak lagi optional
+type RequiredMyUser = Required<MyUser>
+
+//? PICK
+type JustEmailAndName = Pick<MyUser, "email" | "name">
+
+//? RECORD
+const mapById = (users: MyUser[]): Record<string, MyUser> => {
+  return users.reduce((a,v)=> {
+    return {
+      ...a,
+      [v.id]:v
+    }
+  }, {})
+}
+
+console.log(mapById([
+  {
+    id: "firstPersonID",
+    name: "John"
+  },
+  {
+    id: "secondPersonID",
+    name: "Marie"
+  },
+]));
