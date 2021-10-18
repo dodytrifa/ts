929ab08cfbcc3bfee04de4735bd5cac74f2e256f
dodytrifa <dody.trifa@gmail.com>
2021-10-18 22:12:46 +0700

    class finish

--- _nbsts/12-class.ts
+++ _nbsts/12-class.ts
@@ -5,8 +5,12 @@ interface Database {
 
 class InMemoryDatabase implements Database {
   //*visibility
+  //* bentuk awal
   // db: Record<string, string> = {}
-  private db: Record<string, string> = {}
+  //* bentuk sebelum persistent
+  // private db: Record<string, string> = {}
+  //* bentuk akhir
+  protected db: Record<string, string> = {}
   get(id: string): string {
     return this.db[id]
   }
@@ -15,11 +19,32 @@ class InMemoryDatabase implements Database {
   }
 }
 
-
-
 const myDB = new InMemoryDatabase()
 myDB.set("key", "value")
 console.log(myDB.get("key"));//* value
 
 //*karena db diprivate maka tidak bisa diakses
 //* myDB.db["key"] = "access the private"
+
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storedState: string): void
+}
+
+class PersistentMemoryDb extends InMemoryDatabase implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db)
+  }
+  restoreFromString(storedState: string): void {
+    return this.db = JSON.parse(storedState)
+  }
+}
+const myPersistDB = new PersistentMemoryDb()
+console.log("=== PERSISTENT DB vvvvv");
+
+myPersistDB.set("Persistent", "This is the value");
+console.log(myPersistDB.get("Persistent"));
+
+const firstSave = myPersistDB.saveToString();
+
+
