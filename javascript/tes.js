2eefdc605eecefe09a2c2cbb7ad47e34a72f3b3b
dodytrifa <dody.trifa@gmail.com>
2021-11-15 23:00:06 +0700

    just js

--- javascript/tes.js
+++ javascript/tes.js
@@ -1,3 +1,21 @@
-if (typeof(value) === 'date') {
-  console.log('This is a date, indeed!');
+// if (typeof(value) === 'date') {
+//   console.log('This is a date, indeed!');
+// }
+
+let countDwarves = function() {return 7}
+let dwarves = countDwarves
+console.log(dwarves);
+
+console.log(Object.is(5,"5"));
+
+let president = {
+  name: 'Pooh', 
+  next:null
 }
+
+president.next = {
+  name: 'Paddington',
+  next: president
+}
+
+console.log(president.next.next.name);
