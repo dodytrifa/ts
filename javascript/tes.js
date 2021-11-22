161c367f52a0d5e476c6f9e6ad1f5601898ffb3d
dodytrifa <dody.trifa@gmail.com>
2021-11-22 21:45:07 +0700

    continue JJS

--- javascript/tes.js
+++ javascript/tes.js
@@ -19,3 +19,38 @@ president.next = {
 }
 
 console.log(president.next.next.name);
+
+console.log("=====MUTATION====");
+
+let sherlock = {
+  surname: 'Holmes',
+  address: {
+    city: 'London'
+  }
+}
+
+let john = {
+  surname: 'Watson',
+  address: sherlock.address
+}
+
+console.log(sherlock.surname);
+console.log(sherlock.address.city);
+console.log(john.surname);
+console.log(john.address.city);
+
+// mutasi yang akan mengubah property sherlock
+john.surname = "Lennon"
+john.address.city= "Malibu"
+
+console.log("!!! MUTATED !!!");
+console.log(sherlock.address.city);
+console.log(john.address.city);
+
+//LET VS CONST
+const shrek = {
+  species: "ogre"
+}
+
+shrek.species = "Human"
+console.log(shrek.species) //Human
