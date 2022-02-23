87e5144ed360fa13cbd21be92e6257097fd99278
dodytrifa <dody.trifa@gmail.com>
2022-02-23 21:22:04 +0700

    learn utility functions

---
+++ _nbsts/19-utility.ts
@@ -0,0 +1,44 @@
+type Name = {
+  first: string;
+  last: string;
+}
+
+function addFullName(name: Name) : Name & {
+  fullName: string
+}{
+  return {
+    ...name,
+    fullName: `${name.first} ${name.last}`
+  }
+}
+
+function permuteRows<T extends (...args:any[])=>any>(
+  iteratorFunc: T, 
+  data: Parameters<T>[0][]
+  ): ReturnType<T>[]{ 
+    return data.map(iteratorFunc)
+  }
+
+  console.log(permuteRows(addFullName, [
+    {first: "Christiano", last: "Ronaldo"}
+  ]));
+  
+
+  class PersonWithFullName{
+    constructor(public name: Name){}
+
+    get fullName(){
+      return `${this.name.first} ${this.name.last}`
+    }
+  }
+
+  function createObject<T extends new (...args:any[])=>any>(
+    ObjectType: T, 
+    data:ConstructorParameters<T>[0][])
+    : InstanceType<T>[]{
+      return data.map((item) => new ObjectType(item))
+    }
+
+    console.log(createObject(PersonWithFullName, [
+      {first: "ROberto", last: "Carlos"}
+    ]).map(obj => obj.fullName));
