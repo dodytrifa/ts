93690dc825415f2179462106b97656ac7ddc0ba0
dodytrifa <dody.trifa@gmail.com>
2022-03-25 22:51:40 +0700

    string manipulation

--- exec/stringArr.ts
+++ exec/stringArr.ts
@@ -13,3 +13,20 @@ function takesAnArrayOfStrings(anArrayOfStrings: string[]) {
 }
 
 
+// function last<T>(elem: Array<T>){
+//   return elem[elem.length-1]
+// }
+// let n = last<boolean>([true, false]);
+// let n2: number = n;
+
+// n2
+// console.log(n2);
+ 
+function first<T>(elements: Array<T>): T {
+  return elements[0];
+}
+let results: [string, number] = [
+  first<string>(['a', 'b']),
+  first<number>([1, 2]),
+];
+console.log(results);// ['a',1]
