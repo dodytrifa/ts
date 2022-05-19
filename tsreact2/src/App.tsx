e7231620f8a2044131486702fea844fcc79b9df8
dodytrifa <dody.trifa@gmail.com>
2022-05-19 21:39:53 +0700

    hooks with ts

--- tsreact2/src/App.tsx
+++ tsreact2/src/App.tsx
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
 
 //1 create component with prop
@@ -14,18 +14,57 @@ const MyComponent= ({children}:{children: React.ReactNode}) => (
     </div>
 ) 
 
-// const ListSample: React.FunctionComponent = () =>(
-//   <ul>
+const ListSample: React.FunctionComponent<{
+  items: string[];
 
-//   </ul>
-// )
+}> = ({items}) =>(
+  <ul>
+    {items.map((item, index) => (
+      <li key={index}>{item}</li>
+    ))}
+  </ul>
+)
+
+const AlertList: React.FunctionComponent<{
+  figures: number[];
+  onClick?: (figure: number) => void
+}> = ({figures, onClick})=>(
+  <ul>
+  {figures.map((figure, index)=>(
+    <li key={index} onClick={() => onClick?.(figure)}>
+      {figure}
+    </li>
+  ))}
+  </ul>
+) 
+
+interface Payload {
+  text: string
+}
+
 
 function App() {
+  const onAlertList = useCallback((figure:number)=>{
+    alert(figure)
+  },[])
+
+  const [payload, setPayload] = useState<Payload | null>(null)
+
+  useEffect(()=>{
+    fetch("/payload.json")
+    .then(resp => resp.json())
+    .then(data =>{
+      setPayload(data)
+    })
+  })
+
   return (
     <div>
       <Heading title="Introduction"/>
       <MyComponent>"First component with child"</MyComponent>
-      {/* <ListSample></ListSample> */}
+      <ListSample items={["one", "two", "three"]}/>
+      <AlertList figures={[4,5,6]} onClick={onAlertList}/>
+      <MyComponent>{JSON.stringify(payload)}</MyComponent>
     </div>
   );
 }
