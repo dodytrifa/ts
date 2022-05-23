e1591106bb55f03f64c1782e17537e2234064477
dodytrifa <dody.trifa@gmail.com>
2022-05-23 21:13:27 +0700

    prop drilling on ts

--- tsreact2/src/App.tsx
+++ tsreact2/src/App.tsx
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import React, {useCallback, useEffect, useReducer, useState} from 'react';
+import React, {useCallback, useEffect, useReducer, useRef, useState} from 'react';
 import './App.css';
 
 //1 create component with prop
@@ -44,8 +44,7 @@ interface Payload {
 
 interface Todo {
   id:number
-  todo: string
-  quantity: number
+  text: string
   isComplete: boolean
 }
 
@@ -53,6 +52,11 @@ type ActionType =
   | {type: "ADD", text: string}
   | {type: "REMOVE", id:number}
 
+  //#22 prop drilling
+  const Incrementer: React.FunctionComponent<> = () => ({
+    
+  })
+  ///////////////////////////////////////////////////////////////
 function App() {
   const onAlertList = useCallback((figure:number)=>{
     alert(figure)
@@ -65,10 +69,24 @@ function App() {
   const [todos, dispatch] = useReducer((todos: Todo[], action: ActionType)=> {
     switch(action.type){
       case "ADD":
+        return [
+          ...todos,
+          {
+            id: todos.length,
+            text: action.text,
+            isComplete: false,
+          }
+        ]
+      case "REMOVE":
+        return todos.filter(({id})=> id !== action.id)
+      default:
+        throw new Error()
     }
-  }[])
+  },
+  []
+  )
 
-
+// useEffect
   useEffect(()=>{
     fetch("/payload.json")
     .then(resp => resp.json())
@@ -77,6 +95,23 @@ function App() {
     })
   })
 
+  // useRef
+  const newTodoRef = useRef<HTMLInputElement>(null)
+
+  const onAddTodo = useCallback(()=> {
+    if (newTodoRef.current){
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value
+      })
+      newTodoRef.current.value = ""
+    }
+
+  }, [])
+
+  // No 22 useState Prop Drilling
+  const [value, setValue] = useState(0)
+   
   return (
     <div>
       <Heading title="Introduction"/>
@@ -85,6 +120,20 @@ function App() {
       <AlertList figures={[4,5,6]} onClick={onAlertList}/>
       <MyComponent>"Below is sample of usetate of Payload from JSON"</MyComponent>
       <MyComponent>{JSON.stringify(payload)}</MyComponent>
+      <MyComponent>"Below is sample of UseReducer"</MyComponent>
+      {todos.map(todo =>(
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={()=>dispatch({
+            type: "REMOVE",
+            id: todo.id,
+            })}>Remove</button>
+        </div>
+      ))}
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={onAddTodo}>Add Todo</button>
+      </div>
     </div>
   );
 }
