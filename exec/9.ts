2f21deb1dee249424cc5f04eedbd6ab895400703
Dody Tri Fatwadi <74846816+dodytrifa@users.noreply.github.com>
2022-07-28 16:56:55 +0700

    learn make compose argument

---
+++ exec/9.ts
@@ -0,0 +1,17 @@
+export function compose(...args: any[]){
+    return {} as any;
+}
+
+const addOne = (a:number) => {
+    return a + 1
+}
+
+const numToString = (a:number) => {
+    return a.toString()
+}
+
+const stringToNum = (a:string) => {
+    return parseInt(a)
+}
+
+const addOneToString = compose(addOne, numToString, stringToNum)
